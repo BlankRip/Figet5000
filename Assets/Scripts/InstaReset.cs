c2a1900b358348649bc2984629940c583e3a8fa6
BlankRip <aniketh_g5@outlook.com>
2020-05-04 15:50:30 +0400

    Parent Class made, Insta reset switch created

---
+++ Assets/Scripts/InstaReset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstaReset : LightUps
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        initialMat = renderer.material;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        OnTouchDown();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        OnLiftOff();
+    }
+}
---
+++ Assets/Scripts/LightUps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(fileName = "LigtUpSettings", menuName = "LightUp's Settings")]
+public abstract class LightUps : MonoBehaviour
+{
+    [HideInInspector] public Renderer renderer;
+    [HideInInspector] public Material initialMat;
+    public Material changeMatTo;
+
+    public void OnTouchDown()
+    {
+        switchMats(renderer, changeMatTo);
+    }
+
+    public void OnLiftOff()
+    {
+        switchMats(renderer, initialMat);
+    }
+
+    void switchMats(Renderer renderer, Material mat)
+    {
+        renderer.material = mat;
+    }
+}
