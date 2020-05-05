e37134531d608b140ccc204015bbbd547d46bb02
BlankRip <aniketh_g5@outlook.com>
2020-05-05 16:35:15 +0400

    Code Works, but need to mess with prefabs not overcrowd

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] GameObject[] cornerPieces;
+    [SerializeField] GameObject[] edgePieces;
+    [SerializeField] GameObject[] middlePieces;
+
+    GameObject[] cornerSpawnPoints;
+    GameObject[] edgeSpawnPoints;
+    GameObject[] middleSpwanPoints;
+    int pick;
+    int previousPick;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cornerSpawnPoints = GameObject.FindGameObjectsWithTag("CornerTile");
+        middleSpwanPoints = GameObject.FindGameObjectsWithTag("MiddleTile");
+        edgeSpawnPoints = GameObject.FindGameObjectsWithTag("EdgeTile");
+        LoopSelectSpawn(cornerPieces, cornerSpawnPoints);
+        LoopSelectSpawn(edgePieces, edgeSpawnPoints);
+        LoopSelectSpawn(middlePieces, middleSpwanPoints);
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoopSelectSpawn(GameObject[] spawnObjs, GameObject[] spawnPoints)
     {
-        
+        previousPick = 0;
+        foreach(GameObject obj in spawnPoints)
+        {
+            pick = Random.Range(0, spawnObjs.Length);
+            if(pick == previousPick)
+            {
+                if (pick == spawnObjs.Length - 1)
+                    pick--;
+                else
+                    pick++;
+            }
+            previousPick = pick;
+            Instantiate(spawnObjs[pick], obj.transform.position, obj.transform.rotation);
+        }
     }
 }
