f79f330696f50beedb5124ee25d5ed3adc03fc7f
BlankRip <aniketh_g5@outlook.com>
2020-05-15 23:22:24 +0400

    Ball prefab added, bounces around the map

---
+++ Assets/Scripts/TheBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBall : MonoBehaviour
+{
+    [SerializeField] float speed = 10;
+    Vector3 moveDir;
+    Rigidbody rb; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveDir = Random.insideUnitSphere;
+        rb.velocity = moveDir * speed;
+    }
+}
