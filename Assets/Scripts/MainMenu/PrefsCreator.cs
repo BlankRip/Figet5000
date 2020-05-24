f95ecbc3b515db2606627f9fb82af286d55f9106
BlankRip <aniketh_g5@outlook.com>
2020-05-24 17:51:24 +0400

    Added more players, prefabs, work on menu display and player select

--- Assets/Scripts/MainMenu/PrefsCreator.cs
+++ Assets/Scripts/MainMenu/PrefsCreator.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PrefsCreator : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if(!PlayerPrefs.HasKey("GameVolume"))
             PlayerPrefs.SetFloat("GameVolume", 1f);
 
         if(!PlayerPrefs.HasKey("MouseSens"))
             PlayerPrefs.SetFloat("MouseSens", 1.75f);
+
+        if(!PlayerPrefs.HasKey("PlayerIndex"))
+            PlayerPrefs.SetInt("PlayerIndex", 0);
     }
 }
---
+++ Assets/Scripts/MainMenu/SelectingPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectingPlayer : MonoBehaviour
+{
+    [SerializeField] GameObject[] displaySetObjs;
+    int currentSelected;
+    int previousSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSelected = PlayerPrefs.GetInt("PlayerIndex");
+        UpdatePlayer();
+    }
+
+    public void ChangePlayerUp()
+    {
+        if(currentSelected > displaySetObjs.Length)
+            currentSelected++;
+        else
+            currentSelected = 0;
+        
+        UpdatePlayer();
+    }
+
+    public void ChangePlayerDown()
+    {
+        if(currentSelected < 0)
+            currentSelected--;
+        else
+            currentSelected = displaySetObjs.Length - 1;
+
+        UpdatePlayer();
+    }
+
+    public void UpdatePlayer()
+    {
+        displaySetObjs[previousSelected].SetActive(false);
+        displaySetObjs[currentSelected].SetActive(true);
+        previousSelected = currentSelected;
+    }
+}
